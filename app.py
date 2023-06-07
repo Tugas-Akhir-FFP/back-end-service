Another day to fix fwi calculation :)

app.py
@@ -32,10 +32,13 @@ param_grid = {'seasonal': ['additive', 'multiplicative', None],
 
 
 def grid_search(df):
-    print(len(df),"panjang data")
+
+    low = 1235
+    high = 1248
+    print(low, high)
     #Kotawaringin df[1:1451], df[1451:1471]
     #Sidoarjo df[1:2439], df[2439:2459]wewewew
-    train, test = df[1:1451], df[1451:1471]
+    train, test = df[1:high], df[low:high]
     print(len(train))
     print(len(test))
 
@@ -58,7 +61,7 @@ def grid_search(df):
                                 # predictions = model_fit.predict(start=len(train), end = len(train)+len(test)-1)
 
                                 #create predict 20 data
-                                predictions = model_fit.predict(start=1451, end= 1471-1)
+                                predictions = model_fit.predict(start=low, end= high-1)
                                 r2 = r2_score(test, predictions)
                                 mse = mean_squared_error(test, predictions)
                                 rmse = math.sqrt(mse)
@@ -75,19 +78,13 @@ def grid_search(df):
     model_fit = model.fit(smoothing_level=best_params[3], 
                           smoothing_trend=best_params[4], 
                           smoothing_seasonal=best_params[5])
-    predictions = model_fit.predict(start=1451, end= 1471-1)
+    predictions = model_fit.predict(start=low, end= high-1)
     #change prediction value round to 2 decimal
     predictions = np.array(predictions).flatten().astype(int)
     test = np.array(test).flatten().astype(int)
-
-    #print type of data test
-    print(type(test))
-    print(type(predictions))
-    r2 = r2_score(test, predictions)
-    # mse = mean_squared_error(test, predictions)  
     mse = np.square(np.subtract(test,predictions)).mean()
+    r2 = r2_score(test, predictions)
     rmse = math.sqrt(mse)
-    print(np.array(test))
     hasil = {}
     hasil['Hasil'] = predictions.tolist()  
     print("-------- Kalkulasi hasil Grid------------") 
@@ -100,6 +97,13 @@ def grid_search(df):
     print("HASIL Prediksi : " ,predictions)
     print("Data Test : " ,test)
     return predictions.tolist() 
+
+# def normalisasi(data_predict) :
+#     mean = np.mean(data_predict)
+#     std = np.std(data_predict)
+#     normalisasi_predict = (data_predict - mean) / std
+
+#     return normalisasi_predict
 
 def Prediction(df, seasonal, trend, periods, slevel, stren, sseasonal, start, end):
     print(len(df))
@@ -152,161 +156,213 @@ def Prediction(df, seasonal, trend, periods, slevel, stren, sseasonal, start, en
         'rmse' : rmse
     }
 #create function for formula fwi calculation using 4 parameter
-def fwiCalculation(temp, rh, wind, rainfall, month =1):
+def fwiCalculation(Temp, rh, wind, rainfall):
     
-    #convert wind from m/s to km/h
-    wind = wind * 3.6
-    dmc_prev = 2 * rainfall#default value for dmc_prev = 2 * rainfall
-    dc_prev = 5 * rainfall#default value for dc_prev = 5 * rainfall
-    FFMC_prev = 85 #"Default value"
+    fwi_VALUE = []
 
 
-    ## FFMC SECTION
-    m_prev = 147.2 * (101.0 - FFMC_prev) / (59.5 + FFMC_prev)
-    # Calculate mo
-    if rainfall > 0.5:
-        rf = rainfall - 0.5
-        if m_prev > 150.0:
-            mo = m_prev + (42.5 * rf * math.exp(-100.0 / (251.0 - m_prev)) * (1.0 - math.exp(-6.93 / rf))) + (0.0015 * (m_prev - 150.0) ** 2) * math.sqrt(rf)
-            if mo > 250.0:
-                mo = 250.0
-        else:
-            mo = m_prev + 42.5 * rf * math.exp(-100.0 / (251.0 - m_prev)) * (1.0 - math.exp(-6.93 / rf))
-    else:
-        mo = m_prev
+    ## Set initial Value (Van wagner )
+    # dmc_prev = 6
+    # dc_prev = 15
+    # FFMC_prev = 85 
+    ## Set initial value (Turner and Lawson)
+    dmc_prev = 2 * rainfall[0]
+    dc_prev = 5 * rainfall[0]
+    FFMC_prev = 85
 
-    # Calculate Ed
-    Ed = 0.942 * (rh ** 0.679) + (11.0 * math.exp((rh - 100.0) / 10.0)) + 0.18 * (21.1 - temp) * (1.0 - 1.0 / math.exp(0.115 * rh))
-
-    # Calculate m
-    if Ed > mo:
-        Ew = 0.618 * (rh ** 0.753) + (10.0 * math.exp((rh - 100.0) / 10.0)) + 0.18 * (21.1 - temp) * (1.0 - 1.0 / math.exp(0.115 * rh))
-        if mo < Ew:
-            k1 = 0.424 * (1.0 - (1.0 - rh / 100.0) ** 1.7) + (0.0694 * math.sqrt(wind)) * (1.0 - (1.0 - rh / 100.0) ** 8)
-            kw = k1 * (0.581 * math.exp(0.0365 * temp))
-            m = Ew - (Ew - mo) * math.exp(-kw)
-        else:
-            m = mo
-
-    else:
-        k0 = 0.424 * (1.0 - ((rh / 100.0) ** 1.7)) + (0.0694 * math.sqrt(wind)) * (1.0 - ((rh / 100.0)) ** 8)
-        kd = k0 * (0.581 * math.exp(0.0365 * temp))
-        m = Ed + (mo - Ed) * 10**(-kd)
-
-    # Calculate FFMC
-    FFMC = (59.5 * (250.0 - m)) / (147.2 + m)
     
-    # # Constants
-    # el = [6.5, 7.5, 9.0, 12.8, 13.9, 13.9, 12.4, 10.9, 9.4, 8.0, 7.0, 6.0]
-    # t = 0.5 * el[month-1]
+
+    for i in range(len(Temp)):
+        #set current value
+        current_temp = Temp[i]
+        current_rh = rh[i]
+        current_wind = wind[i]
+        current_rainfall = rainfall[i]
+        #convert wind from m/s to km/h
+        windkmh = current_wind * 3.6
 
 
-    ## DMC SECTION
+        ## FFMC SECTION
+        m_prev = 147.2 * (101.0 - FFMC_prev) / (59.5 + FFMC_prev)
+        # Calculate mo
+        if current_rainfall > 0.5:
+            rf = current_rainfall - 0.5
+            if m_prev > 150.0:
+                mo = m_prev + (42.5 * rf * math.exp(-100.0 / (251.0 - m_prev)) * (1.0 - math.exp(-6.93 / rf))) + (0.0015 * (m_prev - 150.0) ** 2) * math.sqrt(rf)
+                if mo > 250.0:
+                    mo = 250.0
+            else:
+                mo = m_prev + 42.5 * rf * math.exp(-100.0 / (251.0 - m_prev)) * (1.0 - math.exp(-6.93 / rf))
+        else:
+            mo = m_prev
+
+        # Calculate Ed
+        Ed = 0.942 * (current_rh ** 0.679) + (11.0 * math.exp((current_rh - 100.0) / 10.0)) + 0.18 * (21.1 - current_temp) * (1.0 - 1.0 / math.exp(0.115 * current_rh))
+
+        # Calculate m
+        if Ed > mo:
+            Ew = 0.618 * (current_rh ** 0.753) + (10.0 * math.exp((current_rh - 100.0) / 10.0)) + 0.18 * (21.1 - current_temp) * (1.0 - 1.0 / math.exp(0.115 * current_rh))
+            if mo < Ew:
+                k1 = 0.424 * (1.0 - (1.0 - current_rh / 100.0) ** 1.7) + (0.0694 * math.sqrt(windkmh)) * (1.0 - (1.0 - current_rh / 100.0) ** 8)
+                kw = k1 * (0.581 * math.exp(0.0365 * current_temp))
+                m = Ew - (Ew - mo) * math.exp(-kw)
+            else:
+                m = mo
+
+        else:
+            k0 = 0.424 * (1.0 - ((current_rh / 100.0) ** 1.7)) + (0.0694 * math.sqrt(windkmh)) * (1.0 - ((current_rh / 100.0)) ** 8)
+            kd = k0 * (0.581 * math.exp(0.0365 * current_temp))
+            m = Ed + (mo - Ed) * 10**(-kd)
+
+        # Calculate FFMC
+        FFMC = (59.5 * (250.0 - m)) / (147.2 + m)
+
         
-    #Set default Day Lenght = 12 for indonesia
-    Le = 12
-    # Calculate mth = K for DMC
-    if temp < -1.1:
-        temp = -1.1
-        mth = 1.894 * (temp + 1.1) * (100 - rh) * Le * 10 ** (-6)
-    else:
-        mth = 1.894 * (temp + 1.1) * (100 - rh) * Le * 10 ** (-6)
+        # # Constants
+        # el = [6.5, 7.5, 9.0, 12.8, 13.9, 13.9, 12.4, 10.9, 9.4, 8.0, 7.0, 6.0]
+        # t = 0.5 * el[month-1]
 
-    # Calculate dmc with rainfall effect
-    if rainfall > 1.5:
-        rw = 0.92 * rainfall - 1.27
-        wmi = 20.0 + math.exp(5.6348 - ((dmc_prev/ 43.43)))
 
-        if dmc_prev <= 33.0:
-            b = 100.0 / (0.5 + 0.3 * dmc_prev)
-        elif dmc_prev <= 65.0:
-            b = 14.0 - 1.3 * math.log(dmc_prev)
+        ## DMC SECTION
+            
+        #Set default Day Lenght = 12 for indonesia
+        Le = 12
+        # Calculate mth = K for DMC
+        if current_temp < -1.1:
+            current_temp = -1.1
+            mth = 1.894 * (current_temp + 1.1) * (100 - current_rh) * Le * 10 ** (-6)
         else:
-            b = 6.2 * math.log(dmc_prev) - 17.2
+            mth = 1.894 * (current_temp + 1.1) * (100 - current_rh) * Le * 10 ** (-6)
 
-        #MRT Section
-        wmr = wmi + 1000 * rw / (48.77 + b * rw)
-        pr = 244.72-43.43 * math.log(wmr - 20.0)
-        dmc = pr + 100.0 * mth
+        # Calculate dmc with rainfall effect
+        if current_rainfall > 1.5:
+            rw = 0.92 * current_rainfall - 1.27
+            wmi = 20.0 + math.exp(5.6348 - ((dmc_prev/ 43.43)))
+
+            if dmc_prev <= 33.0:
+                b = 100.0 / (0.5 + 0.3 * dmc_prev)
+            elif dmc_prev <= 65.0:
+                b = 14.0 - 1.3 * math.log(dmc_prev)
+            else:
+                b = 6.2 * math.log(dmc_prev) - 17.2
+
+            #MRT Section
+            wmr = wmi + 1000 * rw / (48.77 + b * rw)
+            pr = 244.72-43.43 * math.log(wmr - 20.0)
+            dmc = pr + 100.0 * mth
+            
+        else:
+            pr = dmc_prev
+            if pr < 0.0:
+                pr = 0.0
+            dmc = pr + 100.0 * mth
+        print("DMC for line 237: ", dmc)
+
         
-    else:
-        pr = dmc_prev
-        if pr < 0.0:
-            pr = 0.0
-        dmc = pr + 100.0 * mth
-    
-    ## DC SECTION
-    ## Calculate DC
-    if rainfall > 2.8:
-        Pd = 0.83 * rainfall - 1.27
-        ## Calculate Qprev
-        Qprev = 800 * math.exp(-dc_prev / 400)
+        ## DC SECTION
+        ## Calculate DC
+        if current_rainfall > 2.8:
+            Pd = 0.83 * current_rainfall - 1.27
+            ## Calculate Qprev
+            Qprev = 800 * math.exp(-dc_prev / 400)
 
-        ## Calculate Qrt
-        Qrt = Qprev + 3.937 * Pd
+            ## Calculate Qrt
+            Qrt = Qprev + 3.937 * Pd
 
-        ## Calculate DCrt
-        Dcrt = 400 * math.log(800 / Qrt)
+            ## Calculate DCrt 
+            Dcrt = 400 * math.log(800 / Qrt)
 
-        if Dcrt <= 0:
-            Dcrt = 0
-    else:
-        Dcrt = dc_prev
-
-    ## Calculate V
-    Lf = 12 #set day length default value for indonesia
-    if temp < -2.8:
-        temp = -2.8
-    V = 0.36 * (temp + 2.8) + Lf
-    if V <= 0:
-        V = 0.0
-
-    ## Calculate DC
-    DC = Dcrt + 0.5 * V
-    
-    ## BUI SECTION
-    buiprev = 0.4 * DC 
-    if dmc <= buiprev :
-        bui = 0.8 * (dmc * DC) / (dmc + 0.4 * DC)
-    else:
-        bui = dmc - (1 - (0.8 * DC / (dmc + 0.4 * DC))) * (0.92 + (0.0114 * dmc) ** 1.7)
-    
-    ## ISI Section
-    mFuel = 147.2 * (101.0 - FFMC) / (59.5 + FFMC)
-    Fu = math.exp(0.05039 * wind)
-    Ff = 91.9 * math.exp(-0.1386 * mFuel) * (1.0 + (mFuel ** 5.31) / (4.93 * (10 ** 7)))
-    isi = Ff * Fu* 0.208
-
-
-    ## FWI  SECTION
-    bui = 82
-    if bui <= 80.0:
-        fD = 0.626 * (bui ** 0.809) + 2.0
-        bScale = 0.1 * isi * fD
-        if bScale <= 1.0:
-            fwi = bScale
+            if Dcrt <= 0:
+                Dcrt = 0
         else:
-            fwi = math.exp(2.72 * (0.434 * math.log(bScale)) ** 0.647)
-    else:
-        fD = 1000.0 / (25.0 + 108.64 * math.exp(-0.023 * bui))
-        bScale = 0.1 * isi * fD
-        if bScale <= 1.0:
-            fwi = bScale
-        else:
-            fwi = math.exp(2.72 * (0.434 * math.log(bScale)) ** 0.647)
+            Dcrt = dc_prev
 
-    #Return with round 2 decimalsss
-    return round(fwi,2)
+        ## Calculate V
+        Lf = 12 #set day length default value for indonesia
+        if current_temp < -2.8:
+            current_temp = -2.8
+        V = 0.36 * (current_temp + 2.8) + Lf
+        if V <= 0:
+            V = 0.0
+
+        ## Calculate DC
+        DC = Dcrt + 0.5 * V
+        print("DC for line 263: ", DC)
+        
+        ## BUI SECTION
+        buiprev = 0.4 * DC 
+        if dmc <= buiprev :
+            bui = 0.8 * (dmc * DC) / (dmc + 0.4 * DC)
+        else:
+            bui = dmc - (1 - (0.8 * DC / (dmc + 0.4 * DC))) * (0.92 + (0.0114 * dmc) ** 1.7)
+        print("BUI for line 275: ", bui)
+        
+        ## ISI Section
+        mFuel = 147.2 * (101.0 - FFMC) / (59.5 + FFMC)
+        print("mFuel for line 280: ", mFuel)
+        Fu = math.exp(0.05039 * windkmh)
+        print("Fu for line 282: ", Fu)
+        Ff = 91.9 * math.exp(-0.1386 * mFuel) * (1.0 + (mFuel ** 5.31) / (4.93 * (10 ** 7)))
+        print("Ff for line 282: ", Ff)
+        isi = Ff * Fu* 0.208
+        print("ISI for line 282: ", isi)
+
+
+        ## FWI  SECTION
+        if bui <= 80.0:
+            fD = 0.626 * (bui ** 0.809) + 2.0
+            bScale = 0.1 * isi * fD
+            if bScale <= 1.0:
+                fwi = bScale
+            else:
+                fwi = math.exp(2.72 * (0.434 * math.log(bScale)) ** 0.647)
+        else:
+            fD = 1000.0 / (25.0 + 108.64 * math.exp(-0.023 * bui))
+            bScale = 0.1 * isi * fD
+            if bScale <= 1.0:
+                fwi = bScale
+            else:
+                fwi = math.exp(2.72 * (0.434 * math.log(bScale)) ** 0.647)
+
+        FFMC_prev = FFMC
+        dmc_prev = dmc
+        dc_prev = DC
+
+
+        fwi_VALUE.append(fwi)
+        print("FWI VALUE for line 326: ", fwi_VALUE)
+
+    return [round(value, 1) for value in fwi_VALUE]
 
 def calculate_fwi_list(temperature_list, humidity_list, wind_list, rainfall_list):
-    fwi_list = []
-    for i in range(len(temperature_list)):
-        fwi = fwiCalculation(temperature_list[i], humidity_list[i], wind_list[i], rainfall_list[i])
-        fwi_list.append(fwi)
+    # print("---------LIST SECTION---------")
+    # print(temperature_list)
+    # print(humidity_list)
+    # print(wind_list)
+    # print(rainfall_list)
+    # print("BEFORE CHANGES")
+    # ### Sample data from Canada Journal #####
+    ### NOTEEE : Data dari canada, rumus windkmh dari line 175 ( windkmh = current_wind)
+    # temperature_list = [17, 20, 8.5, 6.5, 13, 6, 5.5, 8.5, 9.5, 7, 6.5, 6, 13, 15.5, 23, 19]
+    # humidity_list = [42, 21, 40, 25, 34, 40, 52, 46, 54, 93, 71, 59, 52, 40, 25, 46]
+    # wind_list = [25, 25, 17, 6, 24, 22, 6, 16, 20, 14, 17, 17, 4, 11, 9, 16]
+    # rainfall_list = [0, 2.4, 0, 0, 0, 0.4, 0, 0, 0, 9, 1, 0, 0, 0, 0, 0]
+    # print("AFTER CHANGES")
+    # print(temperature_list)
+    # print(humidity_list)
+    # print(wind_list)
+    # print(rainfall_list)
+
+    fwi_list = fwiCalculation(temperature_list, humidity_list, wind_list, rainfall_list)
+    # for i in range(len(temperature_list)):
+    #     fwi = fwiCalculation(temperature_list[i], humidity_list[i], wind_list[i], rainfall_list[i])
+    #     fwi_list.append(fwi)
     return fwi_list
 
 def dataProcessing(data, periods, start, end, freq='D'):
     def pre_Fix_data(data) :
+        data = data.replace('', np.nan)
+        data = data.replace(['0'], np.nan)
         data = data.replace(['8888', ''], np.nan)
         data = data.astype(float)
         data = data.fillna(method='ffill').fillna(method='bfill')
@@ -335,7 +391,7 @@ def dataProcessing(data, periods, start, end, freq='D'):
         parameters.append({
             'name': param_name,
             'data': param_data,
-            'trend': None if param_name in ['Temperature', 'Rainfall'] else 'multiplicative' if param_name == 'Humidity' else 'additive',
+            'trend': None if param_name in ['Temperature'] else 'multiplicative' if param_name == 'Humidity' else 'additive' if param_name == 'Rainfall' else 'additive',
             'seasonal': None
         })
 
@@ -343,7 +399,17 @@ def dataProcessing(data, periods, start, end, freq='D'):
 
     # date_list = df_filtered['Date'].tolist()
 
-    
+    ## Grid Search
+    # #show Rainfall data
+    # print("--------- Rainfall Data ---------")
+    # print(parameters[3]['data'])
+
+    # # Grid Search for rainfall
+    # Rainfall_grid = grid_search(parameters[3]['data'])
+
+    # # Show grid Result
+    # print("--------- Grid Search Result ---------")
+    # print(Rainfall_grid)
 
     predict_result = []
     error_result = []